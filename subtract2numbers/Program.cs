efe559c96d2dbac2f62b4904e1ffe1ed6db56bef
shubham bhardwaj <shubhamjha451@gmail.com>
2023-06-09 14:29:42 +0530

    subtract of two number

---
+++ subtract2numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1;
+int num2;
+
+int result;
+
+Console.WriteLine("please enter num1");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("please enter num2");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+result = num1 - num2;
+
+Console.WriteLine(result);
+Console.ReadLine();
+
+
+
+
+
+
